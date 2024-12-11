9a6bcdc8079b2303cacf20c4df7d39cf3d7bdf68
RVLISA <anthonyjc8utlook.com.ar>
2024-12-11 04:42:06 -0500

    eae2

--- target/generated-sources/annotations/Logica/Apoderado_.java
+++ target/generated-sources/annotations/Logica/Apoderado_.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-12-11T04:30:05", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-12-11T04:41:34", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Apoderado.class)
 public class Apoderado_ extends Persona_ {
 
